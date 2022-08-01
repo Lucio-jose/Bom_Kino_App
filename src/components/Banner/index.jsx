dd83d024520e4d2c401163a691bd1175159f2dc3
Lucio-jose <luciocanganjo2@gmail.com>
2022-08-01 21:35:04 +0100

    fixing bug

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -8,8 +8,13 @@ return(
     <>
      <Container>
       <Head/>
-      <div>
-        <div><h1>Ola Mundo</h1></div>
+      <div className="container-title-banner">
+      <div className="Title-banner">
+        <div>
+          <h1>Bem vindo ao site Bom-Kino</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero veniam, totam necessitatibus commodi omnis cupiditate, obcaecati delectus error soluta nisi quidem neque! Ea autem amet, expedita sapiente magnam similique veritatis.</p>
+        </div>
+      </div>
       </div>
     </Container>
     </>
--- src/components/Banner/style.js
+++ src/components/Banner/style.js
@@ -7,4 +7,20 @@ export const Container = styled.header`
  background-image: url(${Img1});
  background-size: cover;
 
+ .container-title-banner{
+    width: 75%;
+    height: 440px;
+    padding-left: 140px;
+    display: flex;
+    align-items: center;
+ }
+
+ .Title-banner{
+   text-align: justify;
+    width: 50%;
+    height: 50vh;
+    padding-left: 10px;
+    color: white;
+ }
+
 `
