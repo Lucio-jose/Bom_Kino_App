d1c966fe0a270489215c77f27f692b95a79d3b63
Lucio-jose <luciocanganjo2@gmail.com>
2022-08-02 16:03:48 +0100

    Clean bug

--- src/components/Destaques/index.jsx
+++ src/components/Destaques/index.jsx
@@ -1,21 +1,10 @@
 import React from "react";
-import{Container} from './style.js'
-import { Link } from "react-router-dom";
-import Head from '../Header/index'
+import{Container} from './style.js';
 
 function Banner(){
 return(
     <>
      <Container>
-      <Head/>
-      <div className="container-title-banner">
-      <div className="Title-banner">
-        <div>
-          <h1>Bem vindo ao site Bom-Kino</h1> 
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero veniam, totam necessitatibus commodi omnis cupiditate, obcaecati delectus error soluta nisi quidem neque! Ea autem amet, expedita sapiente magnam similique veritatis.</p>
-        </div>
-      </div>
-      </div>
     </Container>
     </>
 )
